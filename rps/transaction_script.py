7635a989d6d01b7e0acdb4015c19d25272919381
Shehzan Devani <shehzandevani@gmail.com>
2014-06-26 14:09:06 -0700

    started

---
+++ rps/transaction_script.py
@@ -0,0 +1,15 @@
+class TransactionScript:
+  def __init__(self, **kwargs):
+    self.inputs = "hello"
+
+  def self.run:
+    return "hello"
+
+  def failure(self, error):
+    return {success: False}
+
+  def success(self):
+    return {success: True}
+
+# def hello():
+#   return "hello"
